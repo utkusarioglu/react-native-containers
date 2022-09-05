9517a4152812c3fb94803541492827082f0b93aa
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-05 21:19:56 +0300

    Chore(ccache): Alter ln paths
    - Alter ccache paths to `/usr/bin/...` this is a bit of an
      experimental thing to see whether it works properly with PATH
      variables in react native container.

--- scripts/ln-ccache.sh
+++ scripts/ln-ccache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ln -s ccache /usr/local/bin/gcc
-ln -s ccache /usr/local/bin/g++
-ln -s ccache /usr/local/bin/cc
-ln -s ccache /usr/local/bin/c++
-ln -s ccache /usr/local/bin/clang
-ln -s ccache /usr/local/bin/clang++
+ln -s ccache /usr/bin/gcc
+ln -s ccache /usr/bin/g++
+ln -s ccache /usr/bin/cc
+ln -s ccache /usr/bin/c++
+ln -s ccache /usr/bin/clang
+ln -s ccache /usr/bin/clang++
