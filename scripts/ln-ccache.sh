4180f117d436b7f905d23141a7c154c77176a48c
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-05 18:37:37 +0300

    Feat(cclean): Add compiler caching
    - Add steps to `Dockerfile.android.dev` for installing "ccache"
    - Add steps to copy and run a script that links compiler paths
      recommended by react native documentation
    - Add `scripts/**` to watched paths for changed files in github
      actions workflow.
    - Bump patch version.

---
+++ scripts/ln-ccache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ccache /usr/local/bin/gcc
+ln -s ccache /usr/local/bin/g++
+ln -s ccache /usr/local/bin/cc
+ln -s ccache /usr/local/bin/c++
+ln -s ccache /usr/local/bin/clang
+ln -s ccache /usr/local/bin/clang++
