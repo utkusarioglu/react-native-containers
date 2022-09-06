1a449ccade344dd11467d96d3f9d7681218b649c
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-06 09:45:15 +0300

    Chore(ln): Alter `ln` script
    - Update `ln` script for ccache to no longer try to remove the
      existing compilers in `bin_path`.
    - Bump patch image version.

--- scripts/ln-ccache.sh
+++ scripts/ln-ccache.sh
@@ -2,10 +2,9 @@
 
 bin_path=/usr/local/bin
 
-cp ccache "$bin_path"
+cp ccache "$bin_path/"
 
 for compiler in gcc g++ cc c++ clang clang++;
 do
-  sudo rm -rf "$bin_path/$compiler"
   ln -s ccache "$bin_path/$compiler"
 done
