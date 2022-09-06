5ec319a87d65b270aec7b178d2b162d9850cb7c4
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-06 15:40:04 +0300

    Chore(ln): Alter linking behavior
    - Set `ln` to force linking. This is done to ensure that clang
      linking happens as expected.

--- scripts/ln-ccache.sh
+++ scripts/ln-ccache.sh
@@ -7,6 +7,6 @@ cp /usr/bin/ccache "$bin_path/ccache"
 for compiler in gcc g++ cc c++ clang clang++;
 do
   compiler_path="$bin_path/$compiler"
-  ln -s ccache $compiler_path
+  ln -sf ccache $compiler_path
   echo "Linked ccache to $compiler_path"
 done
