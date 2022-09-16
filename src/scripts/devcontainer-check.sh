beec2a0b5aa93bfc73085d15d29b4a8ad6839692
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-16 08:32:46 +0300

    Feat(devcontainer): Add component check script
    - Add a script to be copied to the devcontainer to check whether
      all the required components have been installed

---
+++ src/scripts/devcontainer-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+required_packages="gh node vim git yarn"
+
+for exec in $required_packages;
+do
+  if [ -z "$(which $exec)" ];
+  then
+    echo "Error: $exec is not available inside the container"
+    exit 1
+  fi
+done
+
+node <<EOF
+console.log("Node runs as expected");
+EOF
