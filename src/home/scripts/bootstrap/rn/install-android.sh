7e48957694c85fae6cfef2c2a0d0d7529b9fddde
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-16 12:10:14 +0000

    12:10:14 UTC

---
+++ src/home/scripts/bootstrap/rn/install-android.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eux
+bash --version
+
+ARGS=(
+  android_home
+  sdk_version
+  android_build_version
+  android_tools_version
+  cmake_version
+  ndk_version
+)
+. ${0%/*}/../linux/parse-args.sh
+
+
+curl -sS https://dl.google.com/android/repository/${sdk_version} -o /tmp/sdk.zip
+mkdir -p ${android_home}/cmdline-tools
+unzip -q -d ${android_home}/cmdline-tools /tmp/sdk.zip
+mv ${android_home}/cmdline-tools/cmdline-tools ${android_home}/cmdline-tools/latest
+rm /tmp/sdk.zip 
+
+yes | sdkmanager --licenses 
+yes | sdkmanager "platform-tools" \
+  "platforms;android-${android_build_version}" \
+  "build-tools;${android_tools_version}" \
+  "cmake;${cmake_version}" \
+  "ndk;${ndk_version}" 
+
+rm -rf ${android_home}/.android \
+
+chmod 777 -R /opt/android
